818ed518da8365024a39b4f65fe200e20de8cf1e
DinoDuckVN <56186498+DinoDuckVN@users.noreply.github.com>
2020-05-12 22:01:28 +0700

    Add files via upload

---
+++ src/DinoVNOwO/Utils/Main.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DinoVNOwO\Utils;
+
+use pocketmine\level\Position;
+use pocketmine\network\mcpe\protocol\PlaySoundPacket;
+use pocketmine\network\mcpe\protocol\SpawnParticleEffectPacket;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase{
+	
+	private static $instance;
+
+	public function onEnable() : void{
+		self::$instance = $this;
+	}
+
+	public static function getInstance() : Main{
+		return self::$instance;
+	}
+	
+	public function sound(Position $pos, string $soundname, int $volume = 1, int $pitch = 1, array $players) : bool{
+		$pk = new PlaySoundPacket();
+		$pk->soundName = $soundname;
+		$pk->x = $pos->x;
+		$pk->y = $pos->y;
+		$pk->z = $pos->z;
+		$pk->volume = 1;
+		$pk->pitch = 1;
+		$this->getServer()->broadcastPacket($players, $pk);
+		return true;
+	}
+
+	public function particle(Position $pos, string $particlename, array $players) : bool{
+		$pk = new SpawnParticleEffectPacket();
+		$pk->position = $pos->asVector3();
+		$pk->particleName = $particlename;
+		$this->getServer()->broadcastPacket($players, $pk);
+		return true;
+	}
+}
